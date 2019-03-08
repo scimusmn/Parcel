ddfa434d89c3db22324280ec809821ffb62f0828
Aaron Heidgerken-Greene <aheidgerken@smm.org>
2019-03-08 12:40:24 -0600

    figuring out flags

--- install/main.sh
+++ install/main.sh
@@ -11,15 +11,66 @@ ACCOUNT=''
 REPO=''
 SETUP_DIR="/boot/setup"
 
-while getopts ':a:r:s:d' option; do
-  case "${option}"
-  in
-  a) ACCOUNT=${OPTARG};;
-  r) REPO=${OPTARG};;
-  s) SETUP_DIR=${OPTARG};;
-  d) OUTPUT="/dev/stdout";;
-  esac
+# while getopts ':a:r:s:d' option; do
+#   case "${option}"
+#   in
+#   a) ACCOUNT=${OPTARG};;
+#   r) REPO=${OPTARG};;
+#   s) SETUP_DIR=${OPTARG};;
+#   d) OUTPUT="/dev/stdout";;
+#   esac
+# done
+
+declare -A flags
+declare -A booleans
+args=()
+
+while [ "$1" ];
+do
+    arg=$1
+    #if the next opt starts with a '-'
+    if [ "${1:0:1}" == "-" ]
+    then
+      # move to the next opt
+      shift
+      rev=$(echo "$arg" | rev) #reverse the string
+
+      #if the next opt is not empty, or begins with a '-', or this opt ends in a ':'
+      if [ -z "$1" ] || [ "${1:0:1}" == "-" ] || [ "${rev:0:1}" == ":" ]
+      then
+        # it is a boolean flag
+        bool=$(echo ${arg:1} | sed s/://g)
+        booleans[$bool]=true
+        #echo \"$bool\" is boolean
+      else
+        # it is a flag with a value
+        value=$1
+        flags[${arg:1}]=$value
+        shift
+      fi
+    else
+      args+=("$arg")
+      shift
+      #echo \"$arg\" is an arg
+    fi
 done
+
+if [ ! -z "${flags['a']}" ]; then
+  ACCOUNT="${flags['a']}"
+fi
+
+if [ ! -z "${flags['r']}" ]; then
+  REPO="${flags['r']}"
+fi
+
+if [ ! -z "${flags['s']}" ]; then
+  SETUP_DIR="${flags['s']}"
+fi
+
+if [ "${booleans['-debug']}" = true ]; then
+  OUTPUT="${flags['o']}"
+fi
+
 
 echo "Logging to ${OUTPUT}" > $OUTPUT
 
@@ -200,7 +251,7 @@ if [[ ! -d "app" ]]; then
   doneWorking
 
   if [[ -f "app/config/install.sh" ]]; then
-    bash app/config/install.sh "${OPTS}" -o $OUTPUT
+    bash app/config/install.sh "$@" -o $OUTPUT
   fi
   cd app
 
