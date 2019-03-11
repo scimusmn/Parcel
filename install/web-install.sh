7c80ef0a141f0d25ddfcd27e499828fa4077de36
Aaron Heidgerken-Greene <aheidgerken@smm.org>
2019-03-10 21:14:02 -0500

    fixing web installer

--- install/web-install.sh
+++ install/web-install.sh
@@ -38,14 +38,43 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 ACCOUNT="scimusmn"
 REPO="SteleLite-AppTemplate"
 
-while getopts ':a:r:' option; do
-  case "${option}"
-  in
-  a) ACCOUNT=${OPTARG};;
-  r) REPO=${OPTARG};;
-  ?) echo "Passing ${option} to child scripts";;
-  esac
+while [ "$1" ];
+do
+    arg=$1
+    #if the next opt starts with a '-'
+    if [ "${1:0:1}" == "-" ]
+    then
+      # move to the next opt
+      shift
+      rev=$(echo "$arg" | rev) #reverse the string
+
+      #if the next opt is empty, or begins with a '-', or this opt ends in a ':'
+      if [ -z "$1" ] || [ "${1:0:1}" == "-" ] || [ "${rev:0:1}" == ":" ]
+      then
+        # it is a boolean flag
+        bool=$(echo ${arg:1} | sed s/://g)
+        booleans[$bool]=true
+        #echo \"$bool\" is boolean
+      else
+        # it is a flag with a value
+        value=$1
+        flags[${arg:1}]=$value
+        shift
+      fi
+    else
+      args+=("$arg")
+      shift
+      #echo \"$arg\" is an arg
+    fi
 done
+
+if [ ! -z "${flags['a']}" ]; then
+  ACCOUNT="${flags['a']}"
+fi
+
+if [ ! -z "${flags['r']}" ]; then
+  REPO="${flags['r']}"
+fi
 
 sudo mkdir -p /usr/local/src/setup
 
